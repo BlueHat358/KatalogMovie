c5bef0828812b00280d8575dae8293c42b363863
aspire <nawank358@gmail.com>
2019-02-05 09:58:53 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/katalogmovie/db/DatabaseContract.java
@@ -0,0 +1,33 @@
+package com.example.katalogmovie.db;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    public static final String TABLE_NAME = "FAVORITE_TABLE"
+    public static final String AUTHORITY = "com.example.katalogmovie";
+    private static final String SCHEME = "content";
+
+    private DatabaseContract(){ }
+
+    public static final class FavoriteColumn extends BaseColumns{
+
+    }
+
+    public static final Uri CONTENT_URI = new Uri.Builder().scheme(SCHEME)
+            .authority(AUTHORITY)
+            .appendPath(TABLE_NAME)
+            .build();
+
+    public static String getColumnString(Cursor cursor, String columnName) {
+        return cursor.getString(cursor.getColumnIndex(columnName));
+    }
+    public static int getColumnInt(Cursor cursor, String columnName) {
+        return cursor.getInt(cursor.getColumnIndex(columnName));
+    }
+    public static long getColumnLong(Cursor cursor, String columnName) {
+        return cursor.getLong(cursor.getColumnIndex(columnName));
+    }
+}
